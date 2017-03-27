de0e6473ef80b3310037a1586909a6abb93f0711
Christopher Dougherty <christophertdoc@gmail>
2017-03-27 07:36:10 -0400

    assignment 22

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,6 +2,7 @@ var animatePoints = function() {
 
     var points = document.getElementsByClassName('point');
 
+/*
     var revealFirstPoint = function() {
         points[0].style.opacity = 1;
         points[0].style.transform = "scaleX(1) translateY(0)";
@@ -21,10 +22,17 @@ var animatePoints = function() {
         points[2].style.transform = "scaleX(1) translateY(0)";
         points[2].style.msTransform = "scaleX(1) translateY(0)";
         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
+    };*/
 
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
+    var revealPoint = function() {
+        for (i = 0; i < 3; i++) {
+          points[i].style.opacity = 1;
+          points[i].style.transform = "scaleX(1) translateY(0)";
+          points[i].style.msTransform = "scaleX(1) translateY(0)";
+          points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+        }
+    }
+
+    revealPoint();
 
 };
